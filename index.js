b9d5d35ca17fccc947dd93bb51c95dfbdef13cb9
ntet2001 <manyamaigor2001@gmail.com>
2022-05-26 12:35:12 +0100

    index.js,css,html modified and hamburger added

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+/**
+ * Hamburger open and close
+ */
 const Hamburger= document.querySelector('.hamburger');
-const navlinks = document.querySelector('header .navbar nav .navlink');
+const navlinks = document.querySelector('header .navlink');
+const links = document.querySelectorAll('header .navlink li a');
+
 Hamburger.addEventListener('click',()=>{
     navlinks.classList.toggle("open");
 })
+for (let index = 0; index < links.length; index++) {
+    links[index].addEventListener('click',()=>{
+        navlinks.classList.remove("open");
+    })
+}
+
